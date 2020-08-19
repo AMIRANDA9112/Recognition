0f27611e8c8d6ef570d2a56fd033b82271658946
AMIRANDA9112 <andreselfm@gmail.com>
2020-08-18 21:05:29 -0500

    Version 2.0 ip video Pedestriand detect error

--- engine/Video_proccesor/get_ipp.py
+++ engine/Video_proccesor/get_ipp.py
@@ -14,16 +14,29 @@ def get_ipp(ip, size, detail):
 
     cap.set(5, 3)
 
+    ret, frame = capture.read()
+
+    back_frame = frame
+
     while True:
 
         ret, frame = capture.read()
-        cv2.resize(frame, (0, 0), fx=size, fy=size)
-        n = person_detection(frame, detail)
 
-        cv2.imshow('Ip Faces Analysis', n)
+        if frame is not None:
 
-        if cv2.waitKey(1) == 27:
-            break
+            cv2.resize(frame, (0, 0), fx=size, fy=size)
+            n = person_detection(frame, detail)
+            back_frame = frame
+            cv2.imshow('Ip Pedestrian Analysis', n)
+
+            if cv2.waitKey(1) == 27:
+                break
+
+        else:
+            cv2.imshow('Fail Ip connection', back_frame)
+
+            if cv2.waitKey(1) == 27:
+                break
 
     capture.release()
 
