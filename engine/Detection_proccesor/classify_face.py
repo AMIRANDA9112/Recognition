9ef73cbf8aa5e46db5576807545d5cdb2a52b8fc
AMIRANDA9112 <andreselfm@gmail.com>
2020-08-19 17:08:54 -0500

    Version 2.0 ip video Pedestriand detect error

--- engine/Detection_proccesor/classify_face.py
+++ engine/Detection_proccesor/classify_face.py
@@ -12,52 +12,52 @@ def classify_face(im, faces):
     :param im: video
     :return: list of face names
     """
+    if im is not None:
+        if im.any():
 
-    if im.any():
+            faces_encoded = list(faces.values())
+            known_face_names = list(faces.keys())
+            img = im
 
-        faces_encoded = list(faces.values())
-        known_face_names = list(faces.keys())
+            face_loc = fr.face_locations(img)
 
-        # img = cv2.resize(im, (0, 0), fx=0.5, fy=0.5)
-        img = im
+            if face_loc:
 
-        face_loc = fr.face_locations(img)
+                unknown_face_encodings = fr.face_encodings(img, face_loc)
 
-        if face_loc:
+                face_names = []
+                for face_encoding in unknown_face_encodings:
+                    # See if the face is a match for the known face(s)
+                    matches = fr.compare_faces(faces_encoded, face_encoding)
+                    name = "Unknown"
 
-            unknown_face_encodings = fr.face_encodings(img, face_loc)
+                    # use the known face with the smallest distance to the new face
+                    face_distances = fr.face_distance(faces_encoded, face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
 
-            face_names = []
-            for face_encoding in unknown_face_encodings:
-                # See if the face is a match for the known face(s)
-                matches = fr.compare_faces(faces_encoded, face_encoding)
-                name = "Unknown"
+                    face_names.append(name)
 
-                # use the known face with the smallest distance to the new face
-                face_distances = fr.face_distance(faces_encoded, face_encoding)
-                best_match_index = np.argmin(face_distances)
-                if matches[best_match_index]:
-                    name = known_face_names[best_match_index]
+                    for (top, right, bottom, left), name in zip(face_loc, face_names):
+                        # Draw a box around the face
+                        cv2.rectangle(img, (left - 20, top - 20),
+                                      (right + 20, bottom + 20),
+                                      (255, 255, 255), 2)
 
-                face_names.append(name)
+                        # Draw a label with a name below the face
+                        cv2.rectangle(img, (left - 20, bottom - 15),
+                                      (right + 20, bottom + 20),
+                                      (255, 255, 255), cv2.FILLED)
 
-                for (top, right, bottom, left), name in zip(face_loc, face_names):
-                    # Draw a box around the face
-                    cv2.rectangle(img, (left - 20, top - 20),
-                                  (right + 20, bottom + 20),
-                                  (255, 255, 255), 2)
+                        font = cv2.FONT_HERSHEY_COMPLEX
+                        cv2.putText(img, name, (left - 20, bottom + 15),
+                                    font, 1.0, (0, 0, 0), 2)
+                    return img
 
-                    # Draw a label with a name below the face
-                    cv2.rectangle(img, (left - 20, bottom - 15),
-                                  (right + 20, bottom + 20),
-                                  (255, 255, 255), cv2.FILLED)
-
-                    font = cv2.FONT_HERSHEY_COMPLEX
-                    cv2.putText(img, name, (left - 20, bottom + 15),
-                                font, 1.0, (0, 0, 0), 2)
+            else:
                 return img
-
         else:
-            return img
+            return "fail"
     else:
         return "fail"
--- engine/Video_proccesor/get_collage.py
+++ engine/Video_proccesor/get_collage.py
@@ -10,7 +10,7 @@ def get_collage(frames, count):
 
     nf = (nf // 3) * 3
 
-    print('Total Frames To Collage = {}'.format(nf))
+    print('Making Collage,Total Frames To Collage = {}'.format(nf))
 
     l1 = nf / 3
     l1 = int(l1)
@@ -21,13 +21,10 @@ def get_collage(frames, count):
     l3 = nf
 
     row1 = np.concatenate(frames[0:l1], axis=1)
-    print(row1)
-    print(frames[l1:l2])
+
     row2 = np.concatenate(frames[l1:l2], axis=1)
-    print(row2)
-    print(frames[l2:l3])
+
     row3 = np.concatenate(frames[l2:l3], axis=1)
-    print(row3)
 
     collage = np.concatenate((row1, row2, row3), axis=0)
     return collage
